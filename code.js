e9308b8361962bd13944d86e2100a1a5097d9021
Luis Márquez <l40m@protonmail.com>
2019-07-01 20:29:27 +0000

    Initial commit

--- code.js
+++ code.js
@@ -1,26 +1,15 @@
-var slideIndex = 1;
-showSlides(slideIndex);
+var timeout;
+function changeText() {
+  var elem = document.getElementById("accion");
 
-function plusSlides(n) {
-  showSlides(slideIndex += n);
+  var date = new Date();
+  var horas = date.getHours();
+  var minutos = (date.getMinutes() < 10 ? '0' : '') + date.getMinutes();
+
+  elem.innerHTML = horas + ":" + minutos + " Almuerzo";
+
+  clearTimeout(timeout);
+  timeout = setTimeout(function() {
+    elem.innerHTML = "";
+  }, 3000);
 }
-
-function currentSlide(n) {
-  showSlides(slideIndex = n);
-}
-
-function showSlides(n) {
-  var i;
-  var slides = document.getElementsByClassName("mySlides");
-  var dots = document.getElementsByClassName("dot");
-  if (n > slides.length) {slideIndex = 1}
-    if (n < 1) {slideIndex = slides.length}
-    for (i = 0; i < slides.length; i++) {
-      slides[i].style.display = "none";
-    }
-    for (i = 0; i < dots.length; i++) {
-      dots[i].className = dots[i].className.replace(" active", "");
-    }
-  slides[slideIndex-1].style.display = "block";
-  dots[slideIndex-1].className += " active";
-} 
